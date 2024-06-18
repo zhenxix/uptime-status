dfc185c7b6221e51291c5fdc32197c17ce57843e
zhenxix <138878437+zhenxix@users.noreply.github.com>
2024-06-18 20:51:53 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,15 +1,14 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'ZhenXI Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm795299722-e55242fbbd86891bd5345178',
+    'm795299784-7612511cd287ccc536ceb70e',
+    'm797117807-62149f92a7f8e2f810403747',
   ],
 
   // 日志天数
@@ -22,7 +21,7 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://ip.zhenxi.site/'
     },
     {
       text: 'GitHub',
@@ -30,7 +29,7 @@ window.Config = {
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://zhenxi.site/'
     },
   ],
 };
